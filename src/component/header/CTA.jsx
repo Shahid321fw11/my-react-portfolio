d3d57c73b18d3a85af0765e5f734f888b0d46c10
Shahid321fw11 <sidansari20@gmail.com>
2023-07-14 22:03:13 +0530

    resume Updated

--- src/component/header/CTA.jsx
+++ src/component/header/CTA.jsx
@@ -4,7 +4,7 @@ const CTA = () => {
   return (
     <div className="cta">
       <a
-        href="https://drive.google.com/file/d/1333ywf4mcwja0vDaAR82yvmT2w9154Nx/view?usp=sharing"
+        href="https://drive.google.com/file/d/1efv9r1zBjTkIZcueGue1pooBERzCaLUa/view?usp=sharing"
         target="_blank"
         className="btn"
         rel="noreferrer"
