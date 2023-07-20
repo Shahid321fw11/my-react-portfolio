7c0bfee9497b6448abe31295ad1e7bf78fb57d93
Shahid321fw11 <sidansari20@gmail.com>
2023-07-20 12:23:26 +0530

    change the resume.

--- src/component/header/CTA.jsx
+++ src/component/header/CTA.jsx
@@ -4,7 +4,7 @@ const CTA = () => {
   return (
     <div className="cta">
       <a
-        href="https://drive.google.com/file/d/1efv9r1zBjTkIZcueGue1pooBERzCaLUa/view?usp=sharing"
+        href="https://drive.google.com/file/d/1J-2ADiCa9os4lSzELVHLs1aCuKN4hYnk/view?usp=sharing"
         target="_blank"
         className="btn"
         rel="noreferrer"
