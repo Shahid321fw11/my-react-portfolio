ca9e8b7e51d2a4244ffeebffd72821bf9aeb09ed
Shahid321fw11 <sidansari20@gmail.com>
2023-09-12 20:05:06 +0530

    checking

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ const App = () => {
       <Header />
       <Nav />
       <About />
-      {/* <PortfolioProfessional /> */}
       <PortfolioProfessional />
       <PortfolioPersonal />
       <Experience />
