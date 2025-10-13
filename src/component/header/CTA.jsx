93f94c0f1bb09bdb7aa9eaabe02c9934b2d8bb7f
SHAHID ANSARI <87431462+Shahid321fw11@users.noreply.github.com>
2025-10-13 18:19:18 +0530

    Update CTA.jsx
    Update resume

--- src/component/header/CTA.jsx
+++ src/component/header/CTA.jsx
@@ -4,7 +4,7 @@ const CTA = () => {
   return (
     <div className="cta">
       <a
-        href="https://drive.google.com/file/d/1ytFLjC1V_6pftD8ffiwDvLDwY-qSWJCj/view?usp=sharing"
+        href="https://drive.google.com/file/d/1A_EABILzCKHpL6HPEWmqxgehljuBIiOY/view?usp=sharing"
         target="_blank"
         className="btn"
         rel="noreferrer"
